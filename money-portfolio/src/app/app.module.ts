5fe7bf2f79bc40ff74912d9c33a7d1637e38135f
pratham <prathamsoni062@email.com>
2024-07-14 17:36:44 +0530

    Base implemented

--- money-portfolio/src/app/app.module.ts
+++ money-portfolio/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './mycomponents/navbar/navbar.component';
+import { DashboardComponent } from './mycomponents/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
