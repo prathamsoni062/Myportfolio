183f09037985aaa21fc69fc769b40e5c17298e1e
Pratham <prathamsoni062@gmail.com>
2024-12-26 00:09:00 +0530

    report implemented

--- money-portfolio/src/app/mycomponents/report/report/report.component.ts
+++ money-portfolio/src/app/mycomponents/report/report/report.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ReportComponent {
 
+  users = [
+    { InvestmentCategory: 'Stocks', InvestmentAmount: 50000, CurrentValue: 67500, ProfitLoss: 17500, ROI: '35%' },
+  { InvestmentCategory: 'Bonds', InvestmentAmount: 30000, CurrentValue: 29400, ProfitLoss: -600, ROI: '-2%' },
+  { InvestmentCategory: 'Mutual Funds', InvestmentAmount: 40000, CurrentValue: 46000, ProfitLoss: 6000, ROI: '15%' },
+  { InvestmentCategory: 'Real Estate', InvestmentAmount: 30000, CurrentValue: 27000, ProfitLoss: -3000, ROI: '-10%' },
+  ];
+
+  columns = ['InvestmentCategory', 'InvestmentAmount', 'CurrentValue', 'ProfitLoss', 'ROI'];
 }
