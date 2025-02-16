767310ed3621b959ff276be7123ef2f30025c141
Pratham <prathamsoni062@gmail.com>
2025-02-16 16:34:29 +0530

    description removed

--- money-portfolio/src/app/mycomponents/investment/components/investment/investment.component.ts
+++ money-portfolio/src/app/mycomponents/investment/components/investment/investment.component.ts
@@ -5,23 +5,22 @@ import { InvestmentService } from '../../services/investment.service';
 @Component({
   selector: 'app-investment',
   templateUrl: './investment.component.html',
-  styleUrl: './investment.component.scss'
+  styleUrl: './investment.component.scss',
 })
 export class InvestmentComponent implements OnInit {
-
-  constructor(private investmentService: InvestmentService) {}  
-  btnLabel = "Add Investment";
-  investmentData:any;
+  constructor(private investmentService: InvestmentService) {}
+  btnLabel = 'Add Investment';
+  investmentData: any;
   users = [];
 
-  columns = ['Date', 'Type', 'Description', 'Amount', 'Current Value', 'Roi'];
+  columns = ['Date', 'Type', 'Amount', 'Current Value', 'Roi'];
   investmentForm!: FormGroup;
   initiateForm() {
     this.investmentForm = new FormGroup({
       currentValue: new FormControl(''),
       totalInvested: new FormControl(''),
       overallROI: new FormControl(''),
-      addInvestmentForm:new FormControl('')
+      addInvestmentForm: new FormControl(''),
     });
   }
 
@@ -29,30 +28,29 @@ export class InvestmentComponent implements OnInit {
     this.getInvestments();
     this.initiateForm();
   }
-  
+
   setAllData() {
     this.investmentForm.patchValue({
       currentValue: this.investmentData.totalCurrentValue,
       totalInvested: this.investmentData.totalInvested,
-      overallROI: this.investmentData.overallROI
+      overallROI: this.investmentData.overallROI,
     });
     // this.investmentForm.get("totalInvested")?.patchValue('200')
   }
-  
-  getInvestments(){
-    this.investmentService.getInvestments().subscribe((res)=>{
+
+  getInvestments() {
+    this.investmentService.getInvestments().subscribe((res) => {
       this.investmentData = res;
       this.setAllData();
       console.log(this.investmentData);
-      this.users = this.investmentData.investments.map((investment:any)=>({
+      this.users = this.investmentData.investments.map((investment: any) => ({
         Date: new Date(investment.investmentDate).toLocaleDateString(),
-        Type:investment.investmentType,
-        Description:'',
-        Amount:investment.investmentAmount,
-        "Current Value":investment.currentValue, 
-        Roi:investment.roi
-      }))
-    })
+        Type: investment.investmentType,
+        // Description: '',
+        Amount: investment.investmentAmount,
+        'Current Value': investment.currentValue,
+        Roi: investment.roi,
+      }));
+    });
   }
 }
-
